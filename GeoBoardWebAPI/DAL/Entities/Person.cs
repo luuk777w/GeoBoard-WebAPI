36195eb409edfad54a433f23d8263e7975d026fd
matthijs110 <matthijs110@users.noreply.github.com>
2020-04-25 18:32:54 +0200

    Update Person.cs

--- GeoBoardWebAPI/DAL/Entities/Person.cs
+++ GeoBoardWebAPI/DAL/Entities/Person.cs
@@ -4,16 +4,36 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GeoBoardWebAPI.DAL.Entities
 {
+    /// <summary>
+    /// Represents the personal details of a <see cref="User"/>.
+    /// </summary>
     public class Person : IAppEntity
     {
+        /// <summary>
+        /// The identifier of these personal details.
+        /// </summary>
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
-        public DateTimeOffset CreatedAt { get; set; }
+
+        /// <summary>
+        /// The first name of this person.
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// The last name of this person.
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// The country in which this person lives.
+        /// </summary>
         public virtual Country Country { get; set; }
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
-        public string Insertions { get; set; }
-        public string Initials { get; set; }
+
+        /// <summary>
+        /// The data and time of when this person was created [not born ;-)].
+        /// </summary>
+        public DateTimeOffset CreatedAt { get; set; }
     }
 }
