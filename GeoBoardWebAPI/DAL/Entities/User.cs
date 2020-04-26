981c63622c98baaad712d4edbd9bb73433ac18df
matthijs110 <matthijs110@users.noreply.github.com>
2020-04-26 13:07:33 +0200

    Zaken omgedraaid

--- GeoBoardWebAPI/DAL/Entities/User.cs
+++ GeoBoardWebAPI/DAL/Entities/User.cs
@@ -8,16 +8,22 @@ using GeoBoardWebAPI.Models;
 namespace GeoBoardWebAPI.DAL.Entities
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
-    public class User : IdentityUser
+    public class User : IdentityUser, IAppEntity
     {
         public static readonly string[] Roles = { "Administrator", "User" };
+
+        Guid IAppEntity.Id
+        {
+            get { return new Guid(Id); }
+            set { this.Id = value.ToString(); }
+        }
         
-        public Person Person { get; set; }
-        public UserSetting Settings { get; set; }
+        public virtual Person Person { get; set; }
+        public virtual UserSetting Settings { get; set; }
 
         /// <summary>
         /// Whether or not this account is (temporarily) locked.
-        /// This value becomes true when the <see cref="LockoutEnd"/> is filled.
+        /// This value becomes true when the <see cref="this.LockoutEnd"/> is filled.
         /// </summary>
         public bool IsLocked => this.LockoutEnd != null;
 
