8ba8e80bfa854849d05e160ad101536c625971f2
matthijs110 <matthijs110@users.noreply.github.com>
2020-05-11 18:03:34 +0200

    Switch van bord (proef)

--- GeoBoardWebAPI/Hubs/BoardHub.cs
+++ GeoBoardWebAPI/Hubs/BoardHub.cs
@@ -14,30 +14,51 @@ using GeoBoardWebAPI.Models;
 using AutoMapper;
 using GeoBoardWebAPI.Controllers;
 using GeoBoardWebAPI.Models.Board;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
 
 namespace GeoBoardWebAPI.Hubs
 {
+    [Authorize]
     public class BoardHub : Hub
     {
         private readonly IMapper _mapper;
 
+        private readonly BoardRepository BoardRepository;
+
         public BoardHub(
-            IMapper mapper
-)
+            IMapper mapper,
+            BoardRepository boardRepository
+        )
         {
             _mapper = mapper;
-        }
-
-        public override Task OnConnectedAsync()
-        {
-            var sessionId = "board-abc";
-
-            return Groups.AddToGroupAsync(Context.ConnectionId, sessionId);
+            BoardRepository = boardRepository;
         }
 
         public async Task CreateBoard(CreateBoardMutateModel model)
         {
             await Clients.All.SendAsync("BoardCreated", model);
+        }
+
+        public async Task<BoardViewModel> SwitchBoard(Guid? fromBoardId, Guid toBoardId)
+        {
+            // Attemp to find the board.
+            var board = await BoardRepository.GetAll().FirstOrDefaultAsync(b => b.Id.Equals(toBoardId));
+
+            if (board == null) {
+                await Clients.Caller.SendAsync("BoardNotFound", toBoardId);
+
+                return null;
+            }
+
+            var userId = new Guid(Context.User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+            if (fromBoardId != null)
+                await Groups.RemoveFromGroupAsync(Context.ConnectionId, fromBoardId.ToString());
+
+            await Groups.AddToGroupAsync(Context.ConnectionId, board.Id.ToString());
+
+            return _mapper.Map<BoardViewModel>(board);
         }
     }
 }
